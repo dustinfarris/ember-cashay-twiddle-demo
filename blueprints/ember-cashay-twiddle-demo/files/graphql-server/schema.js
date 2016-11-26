b54d0050ff8ec0fb5f454ba49f59e4f0a815d93f
Dustin Farris <dustin.farris@gmail.com>
2016-11-26 19:34:28 +0100

    Update schema

--- blueprints/ember-cashay-twiddle-demo/files/graphql-server/schema.js
+++ blueprints/ember-cashay-twiddle-demo/files/graphql-server/schema.js
@@ -1,38 +1,64 @@
 export default function(graphql) {
   const {
-    GraphQLID,
     GraphQLList,
-    GraphQLObjectType,
     GraphQLSchema,
-    GraphQLString
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID,
+    GraphQLNonNull
   } = graphql;
+
+  const Blurb = new GraphQLObjectType({
+    name: 'Blurb',
+    description: 'A musing of a User',
+    fields: () => ({
+      id: { type: GraphQLID },
+      user: { type: User },
+      postedAt: { type: GraphQLString },
+      content: { type: GraphQLString }
+    })
+  });
 
   const User = new GraphQLObjectType({
     name: 'User',
     description: 'A user',
-    fields: {
+    fields: () => ({
       id: { type: GraphQLID },
-      name: { type: GraphQLString }
-    }
+      name: { type: GraphQLString },
+      blurbs: {
+        type: new GraphQLList(Blurb),
+        resolve: ({ id }, _args, { mirage }) => {
+          return mirage.blurbs.where({ userId: id }).models.map(model => model.attrs);
+        }
+      }
+    })
   });
 
-  const query = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        }
-      },
+  const queryType = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: () => ({
       users: {
         type: new GraphQLList(User),
         resolve: (_parent, _args, { mirage }) => {
           return mirage.users.all().models.map(model => model.attrs);
         }
+      },
+      user: {
+        type: User,
+        args: {
+          id: {
+            type: new GraphQLNonNull(GraphQLID),
+            description: 'The user ID for the desired user'
+          }
+        },
+        resolve: (_parent, { id }, { mirage }) => {
+          return mirage.users.find(id).attrs;
+        }
       }
-    }
+    })
   });
 
-  return new GraphQLSchema({ query });
+  return new GraphQLSchema({
+    query: queryType
+  });
 }
